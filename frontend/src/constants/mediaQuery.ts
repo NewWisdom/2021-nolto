110348e57856df4be2b0081031d332d25848090e
zigsong <wldms5764@gmail.com>
2021-09-05 16:03:20 +0900

    feat: 피드 상세 페이지 반응형 구현

--- frontend/src/constants/mediaQuery.ts
+++ frontend/src/constants/mediaQuery.ts
@@ -1,11 +1,13 @@
 export const BREAK_POINTS = {
   MOBILE: '375px',
+  TABLET_SMALL: '576px',
   TABLET: '768px',
   DESKTOP: '1024px',
 };
 
 export const MEDIA_QUERY = {
   MOBILE: `screen and (max-width: ${BREAK_POINTS.MOBILE})`,
+  TABLET_SMALL: `screen and (max-width: ${BREAK_POINTS.TABLET_SMALL})`,
   TABLET: `screen and (max-width: ${BREAK_POINTS.TABLET})`,
   DESKTOP: `screen and (max-width: ${BREAK_POINTS.DESKTOP})`,
 };
